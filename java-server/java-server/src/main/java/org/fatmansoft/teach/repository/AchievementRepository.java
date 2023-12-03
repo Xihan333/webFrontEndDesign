48641c3b7779d5b18f47af3f280f65fb74094eeb
rainnn-w <504537585@qq.com>
2023-12-03 19:02:11 +0800

    update

--- java-server/java-server/src/main/java/org/fatmansoft/teach/repository/AchievementRepository.java
+++
@@ -1,9 +0,0 @@
-package org.fatmansoft.teach.repository;
-
-import org.fatmansoft.teach.models.Score;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AchievementRepository extends JpaRepository<Score, Integer> {
-}
--- java-server/java-server/src/main/java/org/fatmansoft/teach/util/Test.java
+++
@@ -1,8 +0,0 @@
-package org.fatmansoft.teach.util;
-
-public class Test {
-    public static void main(String args[]) {
-        Double d = Double.parseDouble("006.5");
-        System.out.println(d);
-    }
-}
