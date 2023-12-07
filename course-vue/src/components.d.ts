65719afa213cc36f53f7d09fbdfbe6f26273294e
Xihan <907405758@qq.com>
2023-12-07 23:18:12 +0800

    feat: 社会实践表格+新增修改弹窗（不完全版）

--- course-vue/src/components.d.ts
+++ course-vue/src/components.d.ts
@@ -7,8 +7,33 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    ElAside: typeof import('element-plus/es')['ElAside']
+    ElButton: typeof import('element-plus/es')['ElButton']
+    ElCol: typeof import('element-plus/es')['ElCol']
+    ElContainer: typeof import('element-plus/es')['ElContainer']
+    ElDatePicker: typeof import('element-plus/es')['ElDatePicker']
+    ElDialog: typeof import('element-plus/es')['ElDialog']
+    ElFooter: typeof import('element-plus/es')['ElFooter']
+    ElForm: typeof import('element-plus/es')['ElForm']
+    ElFormItem: typeof import('element-plus/es')['ElFormItem']
+    ElHeader: typeof import('element-plus/es')['ElHeader']
+    ElIcon: typeof import('element-plus/es')['ElIcon']
+    ElInput: typeof import('element-plus/es')['ElInput']
+    ElMain: typeof import('element-plus/es')['ElMain']
+    ElMenu: typeof import('element-plus/es')['ElMenu']
+    ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
+    ElPagination: typeof import('element-plus/es')['ElPagination']
+    ElRow: typeof import('element-plus/es')['ElRow']
+    ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
+    ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
+    ElTable: typeof import('element-plus/es')['ElTable']
+    ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
+    ElTabPane: typeof import('element-plus/es')['ElTabPane']
+    ElTabs: typeof import('element-plus/es')['ElTabs']
+    ElUpload: typeof import('element-plus/es')['ElUpload']
     Menu: typeof import('./components/Menu.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    SocialDialog: typeof import('./components/SocialDialog.vue')['default']
   }
 }
--- course-vue/src/views/student/SocialPrac.vue
+++ course-vue/src/views/student/SocialPrac.vue
@@ -1,1 +1,199 @@
 <!-- 社会实践 -->
+<template>
+<div class="social-prac">
+  <div>
+    <!-- 新增，Dialog是弹窗，需要弹窗的页面都要在components文件夹下新增一个xxDialog.vue -->
+    <SocialDialog v-model:show="show" :rowData="currentRowData" :dialogMode="dialogMode" @updateTable="updateTableData" />
+    <el-button @click="handleAdd" color="#6FB6C1" class="add">新 增</el-button>
+    <!-- 查询部分 -->
+    <div class="query">
+      <el-input
+          clearable
+          class="search"
+          v-model="inputSearch"
+          placeholder="搜索主题和团队名称..."
+          @keyup.enter="searchFn"
+        />
+    <el-button type="primary" @click="searchFn" color="#6FB6C1" class="searchBtn">查 询</el-button>
+    </div>
+  </div>
+  <!-- 表格部分 -->
+  <el-table 
+    :data="paginatedTableData" 
+    style="width: 100%">
+      <!-- 实践主题 -->
+      <el-table-column prop="theme" label="实践主题" width="180" />   
+      <!-- 团队名称 -->
+      <el-table-column prop="groupName" label="团队名称" width="150" />   
+      <!-- 时间 -->
+      <el-table-column sortable prop="day" label="时间" width="100" />   
+      <!-- 地点 -->
+      <el-table-column prop="location" label="实践地点" width="150" />   
+      <el-table-column prop="digest" label="摘要" width="180"/>
+      <!-- 实践成果 -->
+      <el-table-column prop="harvest" label="实践成果" width="180" />   
+      <!-- 审核状态，做了筛选 -->
+      <el-table-column
+        prop="statusName"
+        label="审核状态"
+        width="100"
+        :filters="[
+          { text: '已通过', value: '已通过' },
+          { text: '待审核', value: '待审核' },
+          { text: '未通过', value: '未通过' },
+        ]"
+        :filter-method="filterStatus"
+      />
+      <el-table-column label="操作" width="180" >
+        <!-- 操作部分，根据需要修改 -->
+        <template #default="scope">
+          <el-button size="default" @click="handleEdit(scope.row)">
+            查看
+          </el-button>
+          <el-button size="default" @click="handleDel(scope.row.id, scope.$index + (currentPage - 1) * pageSize)">
+            删除
+          </el-button>
+        </template>
+      </el-table-column>
+  </el-table>
+  <!-- 分页相关，可以直接复制 -->
+  <el-row class="pagination">
+      <el-col>
+        <el-pagination
+          background
+          v-model:current-page="currentPage"
+          v-model:page-size="pageSize"
+          :page-sizes="[15, 25, 35, 50]"
+          :pager-count="7"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="searchedTableData.length"
+          @size-change="handleSizeChange"
+        />
+      </el-col>
+    </el-row>
+</div>
+</template>
+
+<script setup>
+import { ref,computed,onMounted } from 'vue'
+import SocialDialog from '../../components/SocialDialog.vue'
+import { ElMessage } from 'element-plus'
+import request from '../../request/axios_config.ts'
+
+const tableData = ref([])
+onMounted(() => {
+  // 发起请求获取当前工作站的报名数据
+  updateTableData()
+})
+const updateTableData = async () => {
+  const res = await request.post('/api/social/getStudentSocial',{
+    data:{}
+  })
+  tableData.value = res.data.data
+}
+// 弹窗的显示
+const show = ref(false)
+
+// 右上角搜索框查询后的数据内容
+const search = ref('')
+const inputSearch = ref('')
+// 根据搜索框内容搜索, 空参搜索的话相当于显示所有
+const searchedTableData = computed(() => tableData.value.filter(
+  //这个item相当于给每一列一个命名，可以不改
+  item =>
+    //空参的情况
+    !search.value ||
+    // 按照主题theme和团队名称groupName搜索，忽略大小写
+    item.theme.toLowerCase().includes(search.value.toLowerCase()) ||
+    item.groupName.toLowerCase().includes(search.value.toLowerCase())
+))
+const searchFn = () => {
+  // 点击查询按钮后才开始查询
+  search.value = inputSearch.value
+}
+
+//筛选审核状态
+const filterStatus = (value,row) => {
+  return row.statusName === value
+}
+
+// 分页相关,不需要修改，直接复制
+// 当前页数, 默认设置为1
+const currentPage = ref(1)
+const pageSize = ref(15)
+const paginatedTableData = computed(() => searchedTableData.value.slice((currentPage.value - 1) * pageSize.value, currentPage.value * pageSize.value))
+const handleSizeChange = () => {
+  currentPage.value = 1
+}
+
+
+// 操作相关
+const currentRowData = ref({})
+const dialogMode = ref('') // 'add' 或 'view'
+const handleAdd = () => {
+  currentRowData.value = {}  // 清空数据
+  dialogMode.value = 'add'
+  show.value = true
+}
+//查看
+const handleEdit = (rowData) => {
+  console.log(rowData)
+  currentRowData.value = rowData
+  dialogMode.value = 'view'
+  show.value = true
+}
+async function handleDel(socialId,index)  {
+  console.log('点击删除，索引为：'+ index + ',目标为' + socialId)
+  // const res = await 不知道的接口
+  // if(res.data.code==200){
+  tableData.value.splice(index, 1) // pinia中也会同步删除, 因为是ref
+     ElMessage({
+       message: '删除成功！',
+       type: 'success',
+       offset: 150
+     })
+  // }
+  // else{
+  //   ElMessage({
+  //     message: '删除失败，请重试！',
+  //     type: 'error',
+  //     offset: 150
+  //   })
+  // }
+}
+
+
+
+
+</script>
+
+<style lang="scss" scoped>
+el-table{
+  text-align: center;
+}
+.add{
+  width: 70px;
+  height: 35px;
+  margin-bottom: 5px;
+  color: white;
+  font-weight: bold;
+}
+.query{
+  float: right;
+  border-right: 0px;
+  .search{
+    border-color: #6FB6C1;
+    margin-left: auto;
+    margin-right: 10px;
+    width: 200px;
+  }
+  .searchBtn{
+    width: 70px;
+    margin-right: 10px;
+    color: white;
+  }
+}
+
+
+
+</style>
