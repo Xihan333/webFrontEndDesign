ccf92d031b19283d7a1a6a4c44b131f51aa933f3
rainnn-w <504537585@qq.com>
2023-12-08 22:37:44 +0800

    feat：前端成就页面

--- course-vue/src/components.d.ts
+++ course-vue/src/components.d.ts
@@ -7,6 +7,7 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    AchievementDialog: typeof import('./components/AchievementDialog.vue')['default']
     ElAside: typeof import('element-plus/es')['ElAside']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCol: typeof import('element-plus/es')['ElCol']
