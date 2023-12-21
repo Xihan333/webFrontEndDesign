079b786537f12a3cdf032ada8a9aaa97261efc0e
Xihan <907405758@qq.com>
2023-12-21 16:57:50 +0800

    feat:学生主页表格

--- course-vue/src/views/student/Homepage.vue
+++ course-vue/src/views/student/Homepage.vue
@@ -16,10 +16,29 @@
             这里放最高分的科目分数的树状图（拟五门）
         </div>
         <div class="achievement">
-
+            <p class="title">成果奖励</p>
+            <el-table border
+                :data="AchievementTable" 
+                style="width: 80%" class="achievementTable">
+                <el-table-column prop="achievementName" label="成就名称" width="auto" align="center" />   
+                <el-table-column prop="type" label="成就类别" width="auto" align="center"/> 
+                <el-table-column prop="level" label="成就级别" width="auto" align="center"/> 
+                <!-- 时间 -->
+                <el-table-column sortable prop="time" label="时间" width="auto" align="center"/>   
+                <el-table-column prop="content" label="内容" width="auto" align="center"/>   
+                </el-table>
         </div>
         <div class="social">
-
+            <p class="title">社会实践</p>
+            <el-table border
+                :data="SocialTable" 
+                style="width: 80%" class="socialTable">
+                <el-table-column prop="theme" label="实践主题" width="auto" />   
+                <el-table-column prop="groupName" label="团队名称" width="auto" />   
+                <el-table-column sortable prop="day" label="时间" width="auto" />   
+                <el-table-column prop="location" label="实践地点" width="auto" />   
+                <el-table-column prop="harvest" label="实践成果" width="auto" />   
+            </el-table>
         </div>
     </div>
 </template>
@@ -40,6 +59,26 @@ const userInfo = ref({
         campusName:'软件学院',
     }
 })
+const AchievementData = ref([])
+const SocialData = ref([])
+
+onMounted(() =>{
+    updateTableData()
+})
+const updateTableData = async () => {
+    const achievement = await request.get('/achievement/getStudentAchievement')
+    const social = await request.get('/social/getStudentSocial')
+    AchievementData.value = achievement.data.data
+    SocialData.value = social.data.data
+}
+const AchievementTable = computed(() => AchievementData.value.filter(
+    item =>
+        item.status===1
+))
+const SocialTable = computed(() => SocialData.value.filter(
+    item =>
+        item.auditStatus===1
+))
 
 const gpa = ref()
 
@@ -99,5 +138,23 @@ const gpa = ref()
     vertical-align:top;
     background-color: white;
     box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+}
+.achievement,.social{
+    width: 90%;
+    margin-top: 10px;
+    margin-left: 10px;
+    margin-bottom: 10px;
+    background-color: white;
+    box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+    .title{
+        padding-top: 20px;
+        margin-left: 20px;
+        font-weight: bold;
+    }
+    .achievementTable,.socialTable{
+        width: 70%;
+        margin-left: 20px;
+        padding-bottom: 20px;
+    }
 }
 </style>
