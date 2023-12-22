d23d1af2285bbe4da6a2f017cc56585f989665aa
3 <1009266163@qq.com>
2023-12-22 15:01:56 +0800

    fix 一些小bug☪

--- course-vue/src/views/common/NotFound.vue
+++ course-vue/src/views/common/NotFound.vue
@@ -1,6 +1,11 @@
-<!-- 页面找不到 -->
 <template>
-    
+    <p>该页面不存在！</p>
 </template>
-<script setup>
-</script>
+<style scoped>
+p{
+    margin: auto;
+    padding-top: 20px;
+    width: 400px;
+    font-size: 20px;
+}
+</style>
