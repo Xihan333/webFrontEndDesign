b4ff066f209790f1b0fb6b179c34e76815566b22
3 <1009266163@qq.com>
2023-12-22 15:36:53 +0800

    fix 上传头像☪

--- course-vue/src/views/common/NotFound.vue
+++ course-vue/src/views/common/NotFound.vue
@@ -1,6 +1,29 @@
 <template>
     <p>该页面不存在！</p>
+    <img :src="avatar" @click="select"/>
+    <input @change="upload" id="upload" type="file" accept="image/*" style="display:none"/>
 </template>
+<script setup>
+import { ref } from "vue";
+
+function select(){
+    document.getElementById('upload').click();
+}
+
+const avatar=ref('')
+avatar.value=localStorage.getItem('id')
+
+function upload(e){
+    let file=e.target.files[0];
+    let reader=new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload=(data)=>{
+        avatar.value=data.target.result;
+        localStorage.setItem('id',data.target.result) //键值对,键名为用户唯一标识
+    }
+}
+</script>
+
 <style scoped>
 p{
     margin: auto;
