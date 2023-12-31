8ff3afe1e1c868d1c6226dd699bfb0bb92cdc83b
rainnn-w <504537585@qq.com>
2023-12-31 17:07:06 +0800

    update

--- java-server/src/main/java/org/fatmansoft/teach/models/student/Score.java
+++ java-server/src/main/java/org/fatmansoft/teach/models/student/Score.java
@@ -6,14 +6,7 @@ import lombok.NoArgsConstructor;
 import org.fatmansoft.teach.models.teacher.TeacherCourse;
 
 import javax.persistence.*;
-/**
- * Score 成绩表实体类  保存成绩的的基本信息信息，
- * Integer scoreId 人员表 score 主键 score_id
- * Student student 关联学生 student_id 关联学生的主键 student_id
- * Course course 关联课程 course_id 关联课程的主键 course_id
- * Integer mark 成绩
- * Integer ranking 排名
- */
+
 @Entity
 @Data
 @NoArgsConstructor
@@ -29,7 +22,6 @@ public class Score {
     @ManyToOne
     @JoinColumn(name = "student_id")
     private Student student;
-    //初步打算让student维护关系
 
     @ManyToOne
     @JoinColumn(name = "teacher_course_id")
